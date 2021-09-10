85dd5f147d8f03e8ba783744b0e919c07d6760e1
JoseLucioS <dragaolucio@gmail.com>
2021-09-10 11:51:25 -0300

    Add: MedicoDao e IMedico. Nada foi testado

--- src/com/ritallopes/entities/Medico.java
+++ src/com/ritallopes/entities/Medico.java
@@ -34,4 +34,20 @@ public class Medico extends Funcionario{
 	public String toString() {
 		return super.toString().concat(" CRM: "+this.crm+", Especialidade: "+this.especialidade);
 	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
 }
