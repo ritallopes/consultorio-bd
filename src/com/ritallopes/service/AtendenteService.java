99829bdf810e0298331cfa9adc43b5a42b7a563a
JoseLucioS <dragaolucio@gmail.com>
2021-09-10 14:07:50 -0300

    Add: cadastrar paciente atraves da classe atendente

--- src/com/ritallopes/service/AtendenteService.java
+++ src/com/ritallopes/service/AtendenteService.java
@@ -1,11 +1,13 @@
 package com.ritallopes.service;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.ritallopes.dao.PacienteDAO;
 import com.ritallopes.entities.Paciente;
 
 public class AtendenteService {
+	Scanner in = new Scanner(System.in);
 
 	public void mostrarTodosPacientes() {
 		
@@ -19,4 +21,45 @@ public class AtendenteService {
 		
 	}
 
+	public void cadastrarPaciente(){
+		System.out.println("Nome do paciente: ");
+		String nome = in.nextLine();
+		System.out.println("CPF do paciente: ");
+		String cpf = in.nextLine();
+		System.out.println("Telefone do paciente: ");
+		String telefone = in.nextLine();
+		System.out.println("Email do paciente: ");
+		String email = in.nextLine();
+		System.out.println("CEP do paciente: ");
+		String cep = in.nextLine();
+		System.out.println("Convenio do paciente: ");
+		String convenio = in.nextLine();
+		System.out.println("Data de cadastro do paciente: ");
+		String dataCadastro = in.nextLine();
+
+		Paciente paciente = new Paciente(cpf, nome,telefone, email, cep, convenio, dataCadastro);
+
+		PacienteDAO pacienteDAO = new PacienteDAO();
+		pacienteDAO.insert(paciente);
+	}
+
+	public void removerPaciente(){
+
+	}
+
+	public void atualizarPaciente(){
+
+	}
+
+	public void agendarConsulta(){
+
+	}
+
+	public void atualizarConsulta(){
+
+	}
+
+	public void desmarcarConsulta(){
+
+	}
 }
